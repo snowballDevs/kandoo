c3e2e9f8883175add9372e71991408a0fcbd0326
isomer04 <isomer008@gmail.com>
2023-06-12 11:10:44 -0400

    Created simple footer components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css'
+import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
-
   return (
-    <h1 className="text-3xl font-bold underline text-center text-red-500">Hello world!</h1> 
-  )
+    <>
+      <h1 className="text-3xl font-bold underline text-center text-red-500">
+        Hello world!
+      </h1>
+      <Footer />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer  style={{
+      position: 'fixed',
+      bottom: 0,
+      left: 0,
+      width: '100%',
+      justifyContent: 'center',
+      textAlign: 'center',
+    }}>
+      <p>&copy; 2023 - Kandoo</p>
+    </footer>
+  );
+}
+
+export default Footer;
