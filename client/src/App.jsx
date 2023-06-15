d75bbb8676c75faa53a5d427ad092dd0a22a4894
Matthew Bramich <matt_bramich@hotmail.com>
2023-06-15 11:45:43 +1000

    created header component, added daisyui plugin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,14 @@ import Footer from "./components/Footer";
 import Header from './components/Header'
 
 function App() {
+
+
+
   return (
     <>
-      <Header/>
+      <Header />     
+
+
       <Footer />
     </>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,21 +1,41 @@
 const Header = () => {
-
+    const Links = [
+        {name: 'BOARDS', link: 'landingPage'},
+    ]
+    
     return (
-        <header className="w-full">
-            <nav className="flex justify-between py-5 px-10 ">
-                <div>
-                    <h2>KANDOO</h2>
-                </div>
-
-                <div>
-                    <ul className="flex gap-4">
-                        <li>Your Boards</li>
-                        <li>Sign In/Out</li>
+        <nav className='navbar bg-indigo-600'>
+            <div className='flex-1'>
+                <a className='btn btn-ghost normal-case text-xl text-slate-50'>KANDOO</a>
+            </div>
+            <div className='flex-none px-4'>
+                <div className='dropdown dropdown-end'>
+                    <label
+                        tabIndex={0}
+                        className='btn px-2 btn-ghost btn-circle avatar'
+                    >
+                        <div className='w-10 rounded-full ' >
+                            <img src='/userplaceholder.png' />
+                        </div>
+                    </label>
+                    <ul
+                        tabIndex={0}
+                        className='menu menu-sm dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'
+                    >
+                        {Links.map(link => {
+                            return (
+                                <li key={link.name}>
+                                    <button className="btn btn-sm btn-ghost" >{link.name}</button>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </div>
-            </nav>
-        </header>
-    )
+            </div>
+        </nav>
+    );
 }
 
 export default Header
+
+
