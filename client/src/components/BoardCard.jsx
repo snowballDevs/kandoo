917c8431f886d809df86e44f52f806ed9881b86b
Matthew Bramich <matt_bramich@hotmail.com>
2023-06-19 12:24:30 +1000

    created base card component for each board

---
+++ client/src/components/BoardCard.jsx
@@ -0,0 +1,12 @@
+const Card = () => {
+    return (
+        <div className=' bg-gray-300 rounded-xl w-72 min-h-16 '>
+            <div className='flex flex-col gap-2 p-6'>
+                <h3 className='font-bold text-lg'>Board 1</h3>
+                <p className='text-xs'>Summary</p>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
