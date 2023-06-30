08e787c9424f5dcc26fd04e75e044efc96500de5
isomer04 <isomer008@gmail.com>
2023-06-29 20:18:54 -0400

    Added column, footer and task components

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer  style={{
-      position: 'fixed',
-      bottom: 0,
-      left: 0,
-      width: '100%',
-      justifyContent: 'center',
-      textAlign: 'center',
-    }}>
+    <footer
+      style={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        width: "100%",
+        justifyContent: "center",
+        textAlign: "center",
+      }}
+    >
       <p>&copy; 2023 - Kandoo</p>
     </footer>
   );
-}
+};
 
 export default Footer;
--- client/src/components/Task.jsx
+++ client/src/components/Task.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Task = ({ task }) => {
   const { title, priority, description } = task;
