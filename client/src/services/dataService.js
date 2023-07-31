6934a198804a35b0578c8249f373b5e2094f5797
Matthew Bramich <matt_bramich@hotmail.com>
2023-07-31 17:09:15 +1000

    added methods for crud operations

--- client/src/services/dataService.js
+++ client/src/services/dataService.js
@@ -6,6 +6,8 @@ const instance = axios.create({
 });
 
 function DataService() {
+
+    //Auth
     this.getUser = function () {
         return instance.get(`/getUser`);
     };
@@ -17,6 +19,49 @@ function DataService() {
     this.login = function (data) {
         return instance.post(`/login`, data);
     };
+
+    // Board 
+    this.getBoards = () => {
+        return instance.get(`/boards`);
+    }
+
+    this.createBoard = (data) => {
+        return instance.post(`/boards`, data)
+    }
+
+    this.deleteBoard = () => {
+        return instance.delete(`/boards${boardId}`)
+    }
+
+    // Task
+    this.createTask = (data) => {
+        return instance.post(`boards/${boardId}/tasks`, data)
+    }
+
+    this.updateTask = (data) => {
+        return instance.put(`/boards/${boardId}/tasks/${taskId}`, data)
+    }
+
+    this.deleteTask = () => {
+        return instance.delete(`/boards/${boardId}/tasks/${taskId}`)
+    }
+
+    // Comments
+    this.createComment = (data) => {
+        return instance.post(`/boards/${boardId}/tasks/${taskId}/comments`, data)
+    }
+
+    this.updateComment = (data) => {
+        return instance.put(`/boards/${boardId}/tasks/${taskId}/comments/${commentId}`, data)
+    }
+
+    this.likeComment = (data) => {
+        return instance.patch(`/boards/${boardId}/tasks/${taskId}/comments/${commentId}`)
+    }
+
+    this.deleteComment = (data) => {
+        return instance.delete(`/boards/${boardId}/tasks/${taskId}/comments/${commentId}`)
+    }
 }
 
 // exporting a new instance, so that there will be only one instance throughout the app
