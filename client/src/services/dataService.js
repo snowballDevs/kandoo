cc3286e981b9237a06dd6122b8dd4d8f5eafd990
Matthew Bramich <matt_bramich@hotmail.com>
2023-07-31 18:19:34 +1000

    updated boardform, added tailwind forms changed methods in dataService to arrow

--- client/src/services/dataService.js
+++ client/src/services/dataService.js
@@ -6,62 +6,48 @@ const instance = axios.create({
 });
 
 function DataService() {
+    // Auth
+    this.getUser = () => instance.get(`/getUser`);
 
-    //Auth
-    this.getUser = function () {
-        return instance.get(`/getUser`);
-    };
+    this.logout = () => instance.get(`/logout`);
 
-    this.logout = function () {
-        return instance.get(`/logout`);
-    };
+    this.login = (data) => instance.post(`/login`, data);
 
-    this.login = function (data) {
-        return instance.post(`/login`, data);
-    };
+    // Board
+    this.getBoards = () => instance.get(`/boards`);
 
-    // Board 
-    this.getBoards = () => {
-        return instance.get(`/boards`);
-    }
+    this.createBoard = (data) => instance.post(`/boards`, data);
 
-    this.createBoard = (data) => {
-        return instance.post(`/boards`, data)
-    }
-
-    this.deleteBoard = () => {
-        return instance.delete(`/boards${boardId}`)
-    }
+    this.deleteBoard = () => instance.delete(`/boards${boardId}`);
 
     // Task
-    this.createTask = (data) => {
-        return instance.post(`boards/${boardId}/tasks`, data)
-    }
+    this.createTask = (data) => instance.post(`boards/${boardId}/tasks`, data);
 
-    this.updateTask = (data) => {
-        return instance.put(`/boards/${boardId}/tasks/${taskId}`, data)
-    }
+    this.updateTask = (data) =>
+        instance.put(`/boards/${boardId}/tasks/${taskId}`, data);
 
-    this.deleteTask = () => {
-        return instance.delete(`/boards/${boardId}/tasks/${taskId}`)
-    }
+    this.deleteTask = () =>
+        instance.delete(`/boards/${boardId}/tasks/${taskId}`);
 
     // Comments
-    this.createComment = (data) => {
-        return instance.post(`/boards/${boardId}/tasks/${taskId}/comments`, data)
-    }
+    this.createComment = (data) =>
+        instance.post(`/boards/${boardId}/tasks/${taskId}/comments`, data);
 
-    this.updateComment = (data) => {
-        return instance.put(`/boards/${boardId}/tasks/${taskId}/comments/${commentId}`, data)
-    }
+    this.updateComment = (data) =>
+        instance.put(
+            `/boards/${boardId}/tasks/${taskId}/comments/${commentId}`,
+            data
+        );
 
-    this.likeComment = (data) => {
-        return instance.patch(`/boards/${boardId}/tasks/${taskId}/comments/${commentId}`)
-    }
+    this.likeComment = (data) =>
+        instance.patch(
+            `/boards/${boardId}/tasks/${taskId}/comments/${commentId}`
+        );
 
-    this.deleteComment = (data) => {
-        return instance.delete(`/boards/${boardId}/tasks/${taskId}/comments/${commentId}`)
-    }
+    this.deleteComment = (data) =>
+        instance.delete(
+            `/boards/${boardId}/tasks/${taskId}/comments/${commentId}`
+        );
 }
 
 // exporting a new instance, so that there will be only one instance throughout the app
