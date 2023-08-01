4fc2acb07653382be9e28937e35106c615aec46a
Matthew Bramich <matt_bramich@hotmail.com>
2023-08-01 13:04:51 +1000

    added delete btn on cards

--- client/src/components/BoardForm.jsx
+++ client/src/components/BoardForm.jsx
@@ -43,16 +43,16 @@ const BoardForm = () => {
             <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
                 <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
                     <img
-                        className='mx-auto h-10 w-auto'
-                        src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
+                        className='mx-auto w-32'
+                        src='/KandooLogoW.png'
                         alt='Your Company'
                     />
-                    <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
+                    <h2 className='mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
                         The start of something special
                     </h2>
                 </div>
 
-                <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+                <div className='mt-6 sm:mx-auto sm:w-full sm:max-w-sm'>
                     <form className='space-y-2' onSubmit={handleSubmit}>
                         <div>
                             <label
@@ -78,20 +78,20 @@ const BoardForm = () => {
                             <div>
                                 <label
                                     htmlFor='description'
-                                    class='block text-sm font-medium leading-6 text-gray-900'
+                                    className='block text-sm font-medium leading-6 text-gray-900'
                                 >
                                     Description
                                 </label>
-                                <div class='mt-2'>
+                                <div className='mt-2'>
                                     <textarea
                                         id='description'
                                         name='description'
                                         rows='3'
                                         onChange={handleChange}
-                                        class='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                                        className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
                                     ></textarea>
                                 </div>
-                                <p class='mt-3 text-sm leading-6 text-gray-600'>
+                                <p className='mt-3 text-sm leading-6 text-gray-600'>
                                     Write a few sentences about your project.
                                 </p>
                             </div>
