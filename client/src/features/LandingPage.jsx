ab3c998645235ea3d240f9d8c9fa60b92eeee4ae
Matthew Bramich <matt_bramich@hotmail.com>
2023-08-08 12:22:43 +1000

    changed button order,color.changed form with modal

--- client/src/features/LandingPage.jsx
+++ client/src/features/LandingPage.jsx
@@ -1,32 +1,41 @@
-import {useContext} from 'react'
-import { ModalContext } from "../contexts/ModalContext/ModalContext"
-import RegisterForm from "./RegisterForm";
+import {useContext} from 'react';
+import {ModalContext} from '../contexts/ModalContext/ModalContext';
+import RegisterForm from './RegisterForm';
 import LoginForm from '../components/LoginForm';
 import Header from '../components/Header';
+import Modal from '../components/Modal';
 
 const LandingPage = () => {
-  const {handleOpen, isModalOpen} = useContext(ModalContext)
+    const {handleOpen, isModalOpen} = useContext(ModalContext);
 
-return (
-    <div>
-        <Header />
-        <div className='hero min-h-screen bg-base-200'>
-            <div className='hero-content text-center'>
-                <div className='max-w-md'>
-                    <h1 className='text-5xl font-bold'>Projects. Together.</h1>
-                    <p className='py-6'>
-                        Organize your team goals and deploy faster
-                    </p>
-                    <button type='button' className='btn btn-primary' data-modal="modal-register" onClick={handleOpen}>
-                        Get Started
-                    </button>
-                    {/* {isModalOpen && <RegisterForm  />} */}
-                    {isModalOpen && <LoginForm  />}
+    return (
+        <div>
+            <Header />
+            <div className='hero min-h-screen bg-base-200'>
+                <div className='hero-content text-center'>
+                    <div className='max-w-md'>
+                        <h1 className='text-5xl font-bold'>
+                            Projects. Together.
+                        </h1>
+                        <p className='py-6'>
+                            Organize your team goals and deploy faster
+                        </p>
+                        <button
+                            type='button'
+                            className='btn btn-primary'
+                            data-modal='modal-register'
+                            onClick={handleOpen}
+                        >
+                            Get Started
+                        </button>
+                        {/* {isModalOpen && <RegisterForm  />} */}
+                    </div>
                 </div>
             </div>
+            <Modal>
+                <LoginForm />
+            </Modal>
         </div>
-    </div>
-
-)
+    );
 };
 export default LandingPage;
--- client/src/features/WorkspaceHeader.jsx
+++ client/src/features/WorkspaceHeader.jsx
@@ -34,14 +34,15 @@ const WorkspaceHeader = ({boardInfo}) => {
                     </p>
                 </div>
                 <div className='flex gap-5 mt-5 md:mt-0 justify-end'>
-                    <button
-                        onClick={() => deleteProject(boardInfo._id)}
-                        className='flex items-center justify-center font-semibold bg-gray-900 hover:bg-red-500 text-gray-100 dark:bg-gray-600 dark:hover:bg-red-500 dark:text-gray-100 rounded w-32 py-2 px-2'
-                    >
-                        <MdDelete className='text-xl mr-2' /> Delete
-                    </button>
                     <button className='flex items-center justify-center font-semibold bg-gray-900 hover:bg-blue-500 text-gray-100 dark:bg-gray-600 dark:hover:bg-blue-500 dark:text-gray-100 rounded w-32 py-2 px-2'>
                         <MdModeEdit className='text-xl mr-2' /> Edit
+                    </button>
+
+                    <button
+                        onClick={() => deleteProject(boardInfo._id)}
+                        className='flex items-center justify-center font-semibold bg-red-500 hover:bg-red-700 text-gray-100 dark:bg-gray-600 dark:hover:bg-red-500 dark:text-gray-100 rounded w-32 py-2 px-2'
+                    >
+                        <MdDelete className='text-xl mr-2' /> Delete
                     </button>
                 </div>
             </div>
