05a4f730e357a2c48d5f08811a6cc134e5e48adc
Matthew Bramich <matt_bramich@hotmail.com>
2023-08-22 11:59:14 +1000

    dnd board

--- client/src/components/SortableTask.jsx
+++ client/src/components/SortableTask.jsx
@@ -2,7 +2,7 @@ import {useSortable} from '@dnd-kit/sortable';
 import {CSS} from '@dnd-kit/utilities';
 import TaskCard from './TaskCard';
 
-const SortableTask = ({task}) => {
+const SortableTask = ({task, id}) => {
     const {
         setNodeRef,
         attributes,
@@ -11,7 +11,7 @@ const SortableTask = ({task}) => {
         transition,
         isDragging,
     } = useSortable({
-        id: task._id,
+        id,
         data: {
             type: 'Task',
         },
