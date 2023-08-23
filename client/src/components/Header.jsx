075211e743674a2faaf86630d1906ba3586eca40
Christopher Jin <52391491+IntelliJinceTech@users.noreply.github.com>
2023-08-23 13:50:44 -0400

    moved current page to navbar rather than its own component (#101)

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -39,6 +39,16 @@ const Header = ({formDisplay}) => {
                         KANDOO
                     </a>
                 </div>
+                <div className="hidden lg:ml-6 lg:flex lg:space-x-8">
+                  {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+                  <h3
+                    className="inline-flex capitalize items-center px-1 pt-1 text-md font-medium text-gray-900"
+                  >
+                    {currentPage !== 'landingPage' && currentPage}
+                  </h3>
+                  
+                </div>
+                {/* user profile icon */}
                 <div className='flex-none px-4'>
                     <div className='dropdown dropdown-end'>
                         <button
@@ -72,6 +82,10 @@ const Header = ({formDisplay}) => {
                     </div>
                 </div>
             </nav>
+
+            {/* {currentPage !== 'landingPage' && (
+                <PageHeading currentPage={currentPage} />
+            )} */}
         </header>
     );
 };
