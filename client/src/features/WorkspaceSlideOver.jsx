d0040b4cadc5a3eab40a21803220387244d5733e
H Grasso: PC <aitch.grasso@gmail.com>
2023-08-23 20:15:41 -0500

    esLint

--- client/src/features/WorkspaceSlideOver.jsx
+++ client/src/features/WorkspaceSlideOver.jsx
@@ -269,8 +269,6 @@ import {Dialog, Transition} from '@headlessui/react';
 import {ToastContainer, toast} from 'react-toastify';
 import {
     BsPlus,
-    BsLink45Deg,
-    BsFillQuestionCircleFill,
     BsXLg,
 } from 'react-icons/bs';
 import dataService from '../services/dataService';
@@ -282,45 +280,8 @@ import TextAreaEditor from '../components/textAreaEditor';
 import Column from '../components/dndcolumn';
 import Task from '../components/Task';
 
-const team = [
-    {
-        name: 'Tom Cook',
-        email: 'tom.cook@example.com',
-        href: '#',
-        imageUrl:
-            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-        name: 'Whitney Francis',
-        email: 'whitney.francis@example.com',
-        href: '#',
-        imageUrl:
-            'https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-        name: 'Leonard Krasner',
-        email: 'leonard.krasner@example.com',
-        href: '#',
-        imageUrl:
-            'https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-        name: 'Floyd Miles',
-        email: 'floyd.miles@example.com',
-        href: '#',
-        imageUrl:
-            'https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    {
-        name: 'Emily Selman',
-        email: 'emily.selman@example.com',
-        href: '#',
-        imageUrl:
-            'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-];
 const WorkspaceSlideOver = ({
-    key,
+    // key,
     taskName,
     taskDetail,
     taskComments,
@@ -333,7 +294,7 @@ const WorkspaceSlideOver = ({
     const [editingMode, setEditingMode] = useState(false);
     const [editedTaskName, setEditedTaskName] = useState(taskName);
     const [editedTaskDetail, setEditedTaskDetail] = useState(taskDetail);
-    const [editedTags, setEditedTags] = useState(tags);
+    // const [editedTags, setEditedTags] = useState(tags);
 
     const toggleEditingMode = () => {
         setEditingMode(!editingMode);
