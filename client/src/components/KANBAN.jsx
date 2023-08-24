95345426292a1b4e8998212db22839e6f9a5e585
Matthew Bramich <matt_bramich@hotmail.com>
2023-08-24 17:39:33 +1000

    added items fetched from the backend

--- client/src/components/KANBAN.jsx
+++ client/src/components/KANBAN.jsx
@@ -25,41 +25,10 @@ import ColumnLane from './ColumnLane';
 import colums from './data';
 import SortableTask from './SortableTask';
 
-const KANBAN = () => {
-    // const defaultAnnouncements = {
-    //     onDragStart(id) {
-    //         console.log(`Picked up draggable item ${id}.`);
-    //     },
-    //     onDragOver(id, overId) {
-    //         if (overId) {
-    //             console.log(
-    //                 `Draggable item ${id} was moved over droppable area ${overId}.`
-    //             );
-    //             return;
-    //         }
+const KANBAN = ({boardInfo}) => {
+    const {columns} = boardInfo;
 
-    //         console.log(
-    //             `Draggable item ${id} is no longer over a droppable area.`
-    //         );
-    //     },
-    //     onDragEnd(id, overId) {
-    //         if (overId) {
-    //             console.log(
-    //                 `Draggable item ${id} was dropped over droppable area ${overId}`
-    //             );
-    //             return;
-    //         }
-
-    //         console.log(`Draggable item ${id} was dropped.`);
-    //     },
-    //     onDragCancel(id) {
-    //         console.log(
-    //             `Dragging was cancelled. Draggable item ${id} was dropped.`
-    //         );
-    //     },
-    // };
-
-    const [items, setItems] = useState(colums);
+    const [items, setItems] = useState(columns);
     const [activeId, setActiveId] = useState(null);
     const recentlyMovedToNewContainer = useRef(false);
 
