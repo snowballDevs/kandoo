1adbc366ca754c559067c543adb5b8bb1fe06c79
Matthew Bramich <matt_bramich@hotmail.com>
2023-09-22 10:17:57 +1000

    change cookie setting for session

--- server/server.js
+++ server/server.js
@@ -47,7 +47,7 @@ app.use(
         }),
         cookie: {
             maxAge: 1000 * 60 * 60 * 24, // Equals 1 day (1 day * 24 hr/1 day * 60 min/1 hr * 60 sec/1 min * 1000 ms / 1 sec),
-            secure: true  /*!!!when in production will need to uncomment this!!!*/
+            // secure: true  /*!!!when in production will need to uncomment this!!!*/
         },
     })
 );
